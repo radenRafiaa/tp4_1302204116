40aa8cf4cd2c9acd910adf336883a9d5c349cb31
unknown <rafialif2907@gmail.com>
2022-03-17 22:27:23 +0700

    First Initialize

---
+++ tp4_1302204116/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp4_1302204116
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
